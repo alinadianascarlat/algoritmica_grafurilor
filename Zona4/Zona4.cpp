43d9da278d07539d3cdd6a79f69eef013283137c
alinadianascarlat <alina.diana.scarlat@gmail.com>
2020-10-12 20:35:01 +0300

    mesaj

--- Zona4/Zona4.cpp
+++ Zona4/Zona4.cpp
@@ -1,36 +1,52 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-int main() {
-	int n, p, i, j, a[201][201], nord = 0, sud = 0, est = 0, vest = 0;
-	cin >> n >> p;
-	for (i = 0; i < n; i++) {
-		for (j = 0; j < n; j++)
-			cin >> a[i][j];
+int main() { 
+	int n,Z,A[201][201],S=0;
+	cin>>n>>Z;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++)
+			cin >> A[i][j];
+	}
+	if (Z == 1) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (i < j && i + j < n + 1)
+					S += A[i][j];
+			}
+		}
+	}
+	if (Z == 2) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (i<j && i + j>n + 1)
+					S += A[i][j];
+			}
+		}
 	}
 
-
-	for (i = 0; i < n; i++) {
-		for (j = 0; j < n; j++)
-			if (i < j && i + j < n - 1)
-				nord += a[i][j];
-			else if (i < j && i + j > n - 1)
-				est += a[i][j];
-			else if (i > j && i + j < n - 1)
-				vest += a[i][j];
-			else if (i > j && i + j > n - 1)
-				sud += a[i][j];
-		if (p == 1)
-			cout << nord;
-		if (p == 2)
-			cout << est;
-		if (p == 3)
-			cout << sud;
-		if (p == 4)
-			cout << vest;
-		return 0;
+	if (Z == 3) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (i > j && i + j > n + 1)
+					S += A[i][j];
+			}
+		}
 	}
+	if (Z == 4) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (i > j && i + j < n + 1)
+					S += A[i][j];
+			}
+		}
+	}
+	cout<<S;
+	return 0; 
 }
+
+
+
+
 
 
 
