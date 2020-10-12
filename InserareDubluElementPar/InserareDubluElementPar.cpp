c8aabab5f38a323a9bace383b52550e4242146c0
alinadianascarlat <alina.diana.scarlat@gmail.com>
2020-10-12 20:34:23 +0300

    Rezolvare probleme

--- InserareDubluElementPar/InserareDubluElementPar.cpp
+++ InserareDubluElementPar/InserareDubluElementPar.cpp
@@ -6,28 +6,29 @@
 #include <iostream>
 
 using namespace std;
+int v[25];
 
 int main() {
-    int n, i, k = 0;
-   // cout << "n=";
-    cin >> n;
-    int v[25];
-    for (i = 0; i < n; i++) {
-       // cout << "v[" << i << "]="; 
-        cin >> v[k];
-        if (v[k] % 2 == 0)
-            k++;
-        k++;
-    }
-    for (i = 0; i < k; i++) {
-        if (v[i] % 2 == 0) {
-            v[i + 1] = v[i] * 2; 
-            i++;
-        }
-    }
-    for (i = 0; i < k; i++) {
-        cout << v[i] << " ";
-    }
-    return 0;
+	int n, i, k = 0;
+	// cout << "n=";
+	cin >> n;
+
+	for (i = 0; i < n; i++) {
+		// cout << "v[" << i << "]="; 
+		cin >> v[k];
+		if (v[k] % 2 == 0)
+			k++;
+		k++;
+	}
+	for (i = 0; i < k; i++) {
+		if (v[i] % 2 == 0) {
+			v[i + 1] = v[i] * 2;
+			i++;
+		}
+	}
+	for (i = 0; i < k; i++) {
+		cout << v[i] << " ";
+	}
+	return 0;
 }
 
